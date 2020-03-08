1251789ff85303b7e9ab83bb9b943a506e544f5f
Thomas Dressel <thomaspdressel@gmail.com>
2020-03-08 14:25:07 -0700

    added more comments.

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,17 @@ fn main() {
 
     // loops need return values in rust
     let number = loop {
+        // creates a new string mutable variable
         let mut input = String::new();
+        // tells you what is going on
         println!("Enter a number.");
+        // tries to get a number from the user
         io::stdin().read_line(& mut input).expect("Failed to read line.");
+        // if you did not supply a good number it tries again
         let input: i32 = match input.trim().parse() {
+            // if its ok then it returns it
             Ok(num) => num,
+            // otherwise it asks again
             Err(_) => continue,
         };
         // ends the loop and returns the number
